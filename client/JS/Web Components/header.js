ad37afaa84cde9813227b9206791ca75696017ad
Justin Tolin <justintolin@Justins-MacBook-Pro.local>
2020-11-18 15:17:44 -0600

    Finished Mobile Header

---
+++ client/JS/Web Components/header.js
@@ -0,0 +1,115 @@
+class main_header extends HTMLElement {
+    constructor() {
+        super();
+
+        this.innerHTML = `
+        <style>
+            header {
+                min-height: 45px;
+                line-height: 45px;
+                padding: 15px 0px;
+                background-color: #06603A;
+            }
+
+            header #top-header {
+                min-height: 45px;
+                line-height: 45px;
+                position: relative;
+                z-index: 2;
+                background-color: #06603A;
+            }
+
+            header #top-header #branding {
+                float: left;
+            }
+
+            header #top-header .hamburger-menu {
+                float: right;
+                margin: .5em 0;
+                cursor: pointer;
+            }
+
+            header #top-header .hamburger-menu div {
+                width: 25px;
+                height: 3px;
+                margin: 5px 0px;
+                background-color: #ffffff;
+                transition: transform 0.5s ease;
+            }
+
+            header .nav-bar {
+                position: absolute;
+                top: 0px;
+                left: 0px;
+                width: 100%;
+                padding: 15px 0;
+                z-index: 1;
+              background-color: #06603A;
+              transform: translateY(0px);
+              transition: transform .25s ease-in;
+            }
+
+            header .nav-bar #nav-links {
+                float: none;
+                text-align: center;
+              }
+
+            header .nav-bar #nav-links li {
+                display: inline;
+                margin: 0 10px;
+            }
+              
+            header .nav-bar #nav-links a:hover {
+                color: #2c2c2c;
+            }
+
+            header .menu-active {
+                transform: translateY(70px);
+            }
+            
+            header .burger-menu-active .line1 {
+                transform: rotate(45deg) translate(5px, 6px);
+            }
+            header .burger-menu-active .line2 {
+                opacity: 0;
+            }
+            header .burger-menu-active .line3 {
+                transform: rotate(-45deg) translate(5px, -6px);
+            }
+        </style>
+        <header>
+            <div class="container">
+                <div id="top-header">
+                    <h2 id="branding">Justin Tolin</h2>
+                    <div class="hamburger-menu">
+                        <div class="line1"></div>
+                        <div class="line2"></div>
+                        <div class="line3"></div>
+                    </div>
+                </div>
+                <div class="nav-bar">
+                    <nav id="nav-links">
+                        <ol>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Resumè</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        `;
+    }
+
+    connectedCallback() {
+        const burgerMenu = this.querySelector('.hamburger-menu');
+        const navBar = this.querySelector('.nav-bar');
+
+        burgerMenu.addEventListener('click', () => {
+            burgerMenu.classList.toggle('burger-menu-active');
+            navBar.classList.toggle('menu-active');
+        });
+    }
+}
+
+window.customElements.define('main-header', main_header);
