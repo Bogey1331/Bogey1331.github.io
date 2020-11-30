a485e07fd2d9492dfda8456d15893c5d114164c3
Bogey1331 <justin.tolin02@outlook.com>
2020-11-30 16:39:15 -0600

    Made changes to Icon Bars, Fixed up Footer

--- JS/Web Components/footer.js
+++ JS/Web Components/footer.js
@@ -1,31 +1,26 @@
-const footer_template = document.createElement('template');
-footer_template.innerHTML = `
-<style>
-    footer {
-        min-height: 30px;
-        padding: calc(2px + .5vw) 0;
-        background-color: #06603A;
-    }
-
-    social-icon-bar {
-        text-align: left;
-    }
-</style>
-<footer>
-    <div class="container">
-        <social-icon-bar-small></social-icon-bar-small>
-        <h4>Copyright &copy;2020</h4>
-    </div>
-    <link rel='stylesheet' href='/CSS Styles/global.css'>
-</footer>
-`;
-
 class main_footer extends HTMLElement {
     constructor() {
         super();
 
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.appendChild(footer_template.content.cloneNode(true));
+        this.innerHTML = `
+        <style>
+            footer {
+                min-height: 30px;
+                padding: calc(2px + .5vw) calc(1px + .25vw);
+                background-color: #06603A;
+            }
+        
+            social-icon-bar {
+                text-align: left;
+            }
+        </style>
+        <footer>
+            <div class="container">
+                <social-icon-bar-small></social-icon-bar-small>
+                <h4>Copyright &copy;2020</h4>
+            </div>
+        </footer>
+        `;
     }
 }
 
